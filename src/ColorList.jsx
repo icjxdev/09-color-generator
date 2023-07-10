5f1c5c3c73badfec8c0cb9dca11d8dce7d8a7d95
Isaac Chew <isaaccjx00@gmail.com>
2023-07-10 18:06:12 +0800

    complete project code

---
+++ src/ColorList.jsx
@@ -0,0 +1,13 @@
+import SingleColor from "./SingleColor";
+import { nanoid } from "nanoid";
+
+const ColorList = ({ colors }) => {
+  return (
+    <section className="colors">
+      {colors.map((color, index) => (
+        <SingleColor key={nanoid()} color={color} index={index} />
+      ))}
+    </section>
+  );
+};
+export default ColorList;
